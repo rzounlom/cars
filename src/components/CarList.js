f287161ab8c833548dffd0753ce7ee57d2dbbb0d
Romaric Zounlome <rzounlom@gmail.com>
2024-08-09 07:56:37 -0400

    feat: add bold feature for car form and car list

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,27 +4,36 @@ import { removeCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector(({ cars: { cars, searchTerm } }) =>
-    cars.filter((car) => car.name.includes(searchTerm))
-  );
+  const { cars, name } = useSelector(({ cars: { cars, searchTerm }, form }) => {
+    const filteredCars = cars.filter((car) => car.name.includes(searchTerm));
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
+  });
 
   const handleRemoveCar = (id) => {
     dispatch(removeCar(id));
   };
 
-  const renderedCars = cars.map((car) => (
-    <div key={car.id} className="panel">
-      <p>
-        {car.name} - ${car.cost}
-      </p>
-      <button
-        className="button is-danger"
-        onClick={() => handleRemoveCar(car.id)}
-      >
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleRemoveCar(car.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
 
   return (
     <div className="car-list">
